daa5a3a374ea12856eb1a7d145253289092e623a
Son Tran <286.trants@gmail.com>
2024-06-11 16:32:24 +0700

    Implement `consts` package - Package consts defines constants that are shared all among packages of framework.

---
+++ consts/consts.go
@@ -0,0 +1,16 @@
+// Copyright (c) 2022-2024 The Focela Authors, All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package consts defines constants that are shared all among packages of framework.
+package consts
+
+const (
+	ConfigNodeNameDatabase        = "database"
+	ConfigNodeNameLogger          = "logger"
+	ConfigNodeNameRedis           = "redis"
+	ConfigNodeNameViewer          = "viewer"
+	ConfigNodeNameServer          = "server"
+	ConfigNodeNameServerSecondary = "httpserver"
+	StackFilterKeyForGoCarp       = "github.com/gocarp/carp/"
+)
